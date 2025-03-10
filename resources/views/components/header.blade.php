cfa24b7d7b66c426d282b4091011b3e65fcb9833
haroldgalang <galangharold56@gmail.com>
2025-03-10 13:23:32 +0800

    update 1

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <p>Accredited dealer of heating, ventilation, air-conditioning, refrigeration (HVAC-R) and original parts.</p>
+    <p>Our Devoted Team</p>
+</div>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
-    <title>Document</title>
+    <title>Envirocool Corporation</title>
 </head>
+
 <body>
+    <x-header />
     {{ $slot }}
 </body>
+
 </html>
